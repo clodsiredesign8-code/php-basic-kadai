54a2c6633b4ad80efc6fc33b733d9e1d0176dcd7
clodsiredesign8-code <clodsire.design.8@gmail.com>
2026-09-01 18:51:22 +0900

    Add kadai_005

---
+++ kadai_005/score.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題005</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    // 合計点を出力
+    echo '合計点：';
+    echo $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+    echo '<br>';
+
+    // 平均点を出力
+    echo '平均点：';
+    echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
+
+    ?>
+  </p>
+</body>
+
+</html>
